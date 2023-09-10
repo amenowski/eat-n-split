5e3482583f12578768eeba1c70ca9f982907ef9a
contactserus@gmail.com <[contact-serus@gmail.com]>
2023-09-10 18:46:50 +0200

    first commit

---
+++ src/FormSplitBill.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Button from "./Button";
+
+export default function FormSplitBill({ selectctedFriend, onSplitBill }) {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  }
+
+  return (
+    <form className="form-split-bill" onSubmit={handleSubmit}>
+      <h2>Split a bill with {selectctedFriend.name}</h2>
+
+      <label>💰 Bill value</label>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
+
+      <label>👧🏿 Your expense</label>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
+
+      <label>👨‍👦 {selectctedFriend.name}'s expense</label>
+      <input value={paidByFriend} type="text" disabled />
+      <Button>Split bill</Button>
+
+      <label>🤑 Who is paying the bill</label>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
+        <option value="user">You</option>
+        <option value="friend">{selectctedFriend.name}</option>
+      </select>
+    </form>
+  );
+}
---
+++ src/FriendsList.js
@@ -0,0 +1,16 @@
+import Friend from "./App";
+
+export default function FriendsList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
